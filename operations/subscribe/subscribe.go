cc9cadacfb62913c767512373869f78d2018d7e2
Mazunin Konstantin <k.mazunin@tinkoff.ru>
2022-03-12 17:32:34 +0300

    Rewrite WebSockets with goroutines

--- operations/subscribe/subscribe.go
+++ operations/subscribe/subscribe.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"sync"
 )
 
 var upgrader = websocket.Upgrader{
@@ -15,47 +16,75 @@ var upgrader = websocket.Upgrader{
 }
 
 type Server struct {
-	Clients map[*websocket.Conn]bool
+	Clients map[*websocket.Conn]*Client
+}
+
+type Client struct {
+	receiveChannel chan *entity.Entity
 }
 
 var (
 	server = Server{
-		make(map[*websocket.Conn]bool),
+		Clients: make(map[*websocket.Conn]*Client),
 	}
 )
 
-func Echo(handleCreate func(entity entity.Entity)) echo.HandlerFunc {
-	return func(config echo.Context) error {
-		connection, _ := upgrader.Upgrade(config.Response(), config.Request(), nil)
+func handleWebSocketMessage(
+	connection *websocket.Conn,
+	handleCreate func(entity entity.Entity),
+	sendUpdateChan chan *entity.Entity,
+	wg *sync.WaitGroup,
+) {
+	defer wg.Done()
+	for {
+		mt, message, err := connection.ReadMessage()
 
-		// TODO: отправлять в 1 соединении
-		for _, v := range entity.Database {
-			item, _ := json.Marshal(v)
-
-			connection.WriteMessage(websocket.TextMessage, item)
+		if err != nil || mt == websocket.CloseMessage {
+			return
 		}
 
-		server.Clients[connection] = true
+		item := new(entity.Entity)
+		json.Unmarshal(message, item)
+		handleCreate(*item)
+
+		sendUpdateChan <- item
+	}
+}
+
+func publishMessageToWebSocket(
+	connection *websocket.Conn,
+	receiveUpdateChan chan *entity.Entity,
+) {
+	for it := range receiveUpdateChan {
+		rawMessage, _ := json.Marshal(it)
+		connection.WriteMessage(websocket.TextMessage, rawMessage)
+	}
+}
+
+func Echo(handleCreate func(entity entity.Entity)) echo.HandlerFunc {
+	return func(config echo.Context) error {
+		connection, err := upgrader.Upgrade(config.Response(), config.Request(), nil)
+
+		if err != nil {
+			return err
+		}
+
+		receiveChannel := make(chan *entity.Entity)
+
+		client := &Client{receiveChannel}
+		server.Clients[connection] = client
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go handleWebSocketMessage(connection, handleCreate, receiveChannel, &wg)
+		go publishMessageToWebSocket(connection, receiveChannel)
+
+		defer close(receiveChannel)
 		defer delete(server.Clients, connection)
 		defer connection.Close()
 
-		for {
-			mt, message, err := connection.ReadMessage()
+		wg.Wait()
 
-			if err != nil || mt == websocket.CloseMessage {
-				return nil
-			}
-
-			item := new(entity.Entity)
-
-			json.Unmarshal(message, item)
-
-			handleCreate(*item)
-
-			for conn := range server.Clients {
-				// TODO: filtration
-				conn.WriteMessage(websocket.TextMessage, message)
-			}
-		}
+		return nil
 	}
 }
